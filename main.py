f5421df50904a7df6880dc7d24248d77571b9f74
roberCO <rcampos76@gmail.com>
2020-02-05 15:43:35 +0100

    Connected to PSI4 to get atoms from proteins

--- main.py
+++ main.py
@@ -27,27 +27,32 @@ for index in range(0, numberAA):
     aminoacids.append(sys.argv[index+3])
 
 
+#call psi4 to get the atoms of the protein
+psi = psiFour.PsiFour()
+
+atoms = psi.getAtomsFromProtein(proteinName)
+
+
+'''
 aC = angleCalculator.AngleCalculator()
 
 #Initial call minifold
 initialAngleConfiguration = aC.generateInitialConfig(aminoacids)
 
-psi = psiFour.PsiFour()
-
-atoms = psi.getAtomsFromProtein(proteinName)
-
 #reache initial configuration of angles
 atoms = aC.rotateAminoacid(atoms, initialAngleConfiguration)
+
+'''
 
 #HARDCODED
 rotationStep = 10
 
 
-for aminoacid in aminoacids:
-    #TODO rotate all combinations of aminoacids and angles
+#This is hardcoded because QFold is going to be used just with two and three aminoacids
+#if it is scale to more aminoacids, it should be necessary to implement a recursive function
 
 
-'''
+
 #rotate x axis
 for x in range (0, 360, roationStep):
 
@@ -57,9 +62,13 @@ for x in range (0, 360, roationStep):
     #rotate y axis
     for y in range (0, 360, rotationStep):
 
-        #rotate z axis
-        for z in range(0, 360, rotationStep):
+        aC.rotateAminoacid(aminoacids[1], 30)
 
-            rotateAtoms = aC.rotateAminoacid(rotateAtoms, z)
-'''
+        #This rotation is only executed with three AAs
+        if(len(aminoacids) == 3)
+            #rotate z axis
+            for z in range(0, 360, rotationStep):
+
+                rotateAtoms = aC.rotateAminoacid(rotateAtoms, z)
+
 
--- psiFour.py
+++ psiFour.py
@@ -1,8 +1,75 @@
+import subprocess
+import pandas as pd
+import re
+
 class PsiFour():
 
-    def __init__(self):
-        print("init call")
-
     def getAtomsFromProtein(self, protein):
-        #call to psi
-        print("getAtomsFromProteins call")
+        
+        #create input file
+        self.createInputFile(protein)
+
+        #execute psi4
+        self.executePsiCommand()
+
+        #read/parse outputfile
+        return self.parsePsiOutputFile(protein)
+
+
+    def createInputFile(self, protein):
+
+        inputFile = open('inputFile.dat', 'w')
+
+        inputFile.write('molecule ' + protein + '{\n')
+        inputFile.write(' pubchem: '+ protein+'\n')
+        inputFile.write('}\n\n')
+
+        inputFile.write('set basis cc-pvdz\n')
+        inputFile.write('set reference rhf\n')
+        inputFile.write('energy(\'scf\')\n')
+
+        inputFile.close()
+
+    def executePsiCommand(self):
+
+        #execute psi4 by command line (it generates the file output.dat with the information)
+        subprocess.run(["psi4", "inputFile.dat", "outputFile.dat"])
+
+    def parsePsiOutputFile(self, protein):
+
+        #Read the output file and write csv with the data
+        #file_csv = open(protein + ".csv","w")
+        with open("outputFile.dat","r") as filehandle:
+
+            isDataLine = False
+            data = []
+            for line in filehandle:
+
+                #if line is an empty string after reading data
+                if (isDataLine and line.isspace()):
+                    break
+                
+                # Data has ------ and it is necessary to avoid it
+                if(isDataLine and not '--' in line):
+
+                    lineChunks = line.split()
+                    data.append(lineChunks)
+
+                    #lineFormated = re.sub('\s+', ' ', line).strip()
+                    #file_csv.write(lineFormated+'\n') 
+
+                if 'Center' in line:
+                    isDataLine = True
+                
+
+        #file_csv.close()
+
+        atoms = pd.DataFrame(data, columns = ["Element", "X", "Y", "Z", "Mass"])
+        
+        return atoms
+
+        #data = pd.read_csv(protein + ".csv", header=None, delim_whitespace=True)
+        #data.columns = ["Element", "X", "Y", "Z", "Mass"]
+        #print(data.shape)
+        #print(type(data))
+        

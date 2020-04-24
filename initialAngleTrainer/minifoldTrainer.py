51704b1a668d8837f2d2308141747933da9143ac
roberCO <rcampos76@gmail.com>
2020-04-24 18:07:10 +0200

    Angle data preparation included

--- initialAngleTrainer/minifoldTrainer.py
+++ initialAngleTrainer/minifoldTrainer.py
@@ -18,6 +18,9 @@ class MinifoldTrainer():
         #HARDCODED paths
         self.extracted_aminoacids_path = '../data/full_under_200.txt'
         self.full_extracted_aminoacids_path = '../data/angles/full_angles_under_200.txt'
+        self.output_path = '../data/angles/outputs.txt'
+        self.input_AA_path = '../data/angles/input_aa.txt'
+        self.input_PSSM_path = '../data/angles/input_pssm.txt'
 
         if not os.path.isfile(inputPath):
             raise IOError('<!> ERROR: %s does not exist!' %inputPath)
@@ -188,6 +191,85 @@ class MinifoldTrainer():
                     f.write("\n[PSI]\n")
                     f.write(stringify(psis[k]))
 
+
+    def angle_data_preparation():
+
+        # Opn file and read text
+        with open(self.full_extracted_aminoacids_path, "r") as f:
+            lines = f.read().split('\n')
+
+        # Scan first n proteins
+        names = []
+        seqs = []
+        psis = []
+        phis = []
+        pssms = []
+
+        # Extract numeric data from text
+        for i,line in enumerate(lines):
+            if len(names) == 601:
+                break
+            # Read each protein separately
+            if line == "[ID]":
+                names.append(lines[i+1])
+            elif line == "[PRIMARY]":
+                seqs.append(lines[i+1])
+            elif line == "[EVOLUTIONARY]":
+                pssms.append(parse_lines(lines[i+1:i+22]))
+            elif lines[i] == "[PHI]":
+                phis.append(parse_line(lines[i+1]))
+            elif lines[i] == "[PSI]":
+                psis.append(parse_line(lines[i+1]))
+
+        input_aa = []
+        input_pssm = []
+        outputs = []
+
+        long = 0 # Counter to ensure everythings fine
+
+        for i in range(len(seqs)): 
+            if len(seqs[i])>17*2:
+                long += len(seqs[i])-17*2
+                for j in range(17,len(seqs[i])-17):
+                # Padd sequence
+                    input_aa.append(onehotter_aa(seqs[i], j))
+                    input_pssm.append(pssm_cropper(pssms[i], j))
+                    outputs.append([phis[i][j], psis[i][j]])
+                    # break
+
+        input_aa = np.array(input_aa).reshape(len(input_aa), 17*2, 22)
+        input_pssm = np.array(input_pssm).reshape(len(input_pssm), 17*2, 21)
+
+        # Save outputs to txt file
+        with open(self.output_path, "w") as f:
+            for o in outputs:
+                f.write(stringify(o)+"\n")
+
+        # Save AAs & PSSMs data to different files (together makes a 3dims tensor)
+        # Will concat later
+        with open(self.input_AA_path, "w") as f:
+            for aas in input_aa:
+                f.write("\nNEW\n")
+                for j in range(len(aas)):
+                    f.write(stringify(aas[j])+"\n")
+
+        with open(self.input_PSSM_path, "w") as f:
+            for k in range(len(input_pssm)):
+                f.write("\nNEW\n")
+                for j in range(len(input_pssm[k])):
+                    f.write(stringify(input_pssm[k][j])+"\n")
+
+    # Helper function to save data to a .txt file
+    def stringify(vec):
+        return "".join(str(v)+" " for v in vec)
+
+    # Helper functions to extract numeric data from text
+    def parse_lines(raw):
+        return np.array([[float(x) for x in line.split(" ") if x != ""] for line in raw])
+
+    def parse_line(line):
+        return np.array([float(x) for x in line.split(" ") if x != ""])
+
     def coords_split(lister, splice):
         # Split all passed sequences by "splice" and return an array of them
         # Convert string fragments to float 
@@ -196,6 +278,52 @@ class MinifoldTrainer():
             coords.append([float(a) for a in split(c, splice)])
         
         return coords
+
+    # Length of masking - 17x2 AAs
+    def onehotter_aa(seq, pos):
+        pad = 17
+        # Pad sequence
+        key = "HRKDENQSYTCPAVLIGFWM"
+        # Van der Waals radius
+        vdw_radius = {"H": 118, "R": 148, "K": 135, "D": 91, "E": 109, "N": 96, "Q": 114,
+                    "S": 73, "Y": 141, "T": 93, "C": 86, "P": 90, "A": 67, "V": 105,
+                    "L": 124, "I": 124, "G": 48, "F": 135, "W": 163, "M": 124}
+        radius_rel = vdw_radius.values()
+        basis = min(radius_rel)/max(radius_rel)
+        # Surface exposure 
+        surface = {"H": 151, "R": 196, "K": 167, "D": 106, "E": 138, "N": 113, "Q": 144,
+                    "S": 80, "Y": 187, "T": 102, "C": 104, "P": 105, "A": 67, "V": 117,
+                    "L": 137, "I": 140, "G": 0, "F": 175, "W": 217, "M": 160}
+        surface_rel = surface.values()
+        surface_basis = min(surface_rel)/max(surface_rel)
+        # One-hot encoding
+        one_hot = []
+        for i in range(pos-pad, pos+pad): # alponer los guiones ya tiramos la seq para un lado
+            vec = [0 for i in range(22)]
+            # mark as 1 the corresponding indexes
+            for j in range(len(key)):
+                if seq[i] == key[j]:
+                    vec[j] = 1
+                    # Add Van der Waals relative radius
+                    vec[-2] = vdw_radius[key[j]]/max(radius_rel)-basis
+                    vec[-1] = surface[key[j]]/max(surface_rel)-surface_basis
+
+                    print('AA: ', key[j], ' VDW radius: ', vec[-2], ' a: ', vdw_radius[key[j]], ' b: ', max(radius_rel), ' c: ', basis)
+                    print('AA: ', key[j], ' surface:    ', vec[-1], ' a: ', surface[key[j]], ' b: ', max(surface_rel), ' c: ', surface_basis, '\n')
+
+            
+            one_hot.append(vec) 
+        
+        return np.array(one_hot)
+
+    #Crops the PSSM matrix
+    def pssm_cropper(pssm, pos):
+        pssm_out = []
+        pad = 17
+        for i,row in enumerate(pssm):
+            pssm_out.append(row[pos-pad:pos+pad])
+        # PSSM is Lx21 - solution: transpose
+        return np.array(pssm_out)
 
     # Could use "Using LinearAlgebra + built-in norm()" but gotta learn Julia
     def norm(vector):

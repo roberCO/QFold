9e4f6d9f54c0809b6f00e8360ce990fdd46e6b10
roberCO <rcampos76@gmail.com>
2020-08-06 17:56:09 +0200

    script to execute experiments modified

--- execute_experiments.sh
+++ execute_experiments.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
 # check the input parameters ($1 path of proteins | $2 initial number of bits | $3 final number of bits)
-if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]; then
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ] || [ "$4" == "" ]; then
     echo "Any parameter is missing"
-    echo "Parameter 1 (proteins path): " $1
-    echo "Parameter 2 (initial number bits): " $2
-    echo "Parameter 3 (final number bits): " $3
+    echo "Parameter 1 (protein name): " $1
+    echo "Parameter 2 (amminoacids): " $2 
+    echo "Parameter 3 (initial number bits): " $3
+    echo "Parameter 4 (final number bits): " $4
 
     exit 1
 fi
 
-NUMBER_CORES=$(grep -c ^processor /proc/cpuinfo)
-
-while read p; do
-    protein="$(cut -d' ' -f1 <<<"$p")"
-    aa="$(cut -d' ' -f2 <<<"$p")"
-    for index in $(seq $2 $3)
-    do
-    
-        $(python main.py $protein $aa $index)
-        echo "Precalculated energies file for protein $protein with $index bits created!\n"
-    done
-done <$1
+for index in $(seq $3 $4)
+do
+    python main.py $1 $2 $index
+    printf "Precalculated energies file for protein $1 with $index bits created!\n\n"
+done

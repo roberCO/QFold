e30704973924c418bab14b8298913780c7dd00d8
roberCO <rcampos76@gmail.com>
2022-01-31 10:32:38 +0100

    remove hardcoded path

--- initialAngleTrainer/initialAngleTrainer.py
+++ initialAngleTrainer/initialAngleTrainer.py
@@ -1,7 +1,7 @@
 import sys
 import minifoldTrainer
 import sys 
-sys.path.insert(1, '/Users/pabloantoniomorenocasares/Documents/GitHub/QFold/')
+sys.path.insert(1, '../QFold/')
 import utils
 
 if len(sys.argv) == 2 and sys.argv[1] == 'list_trainers':
